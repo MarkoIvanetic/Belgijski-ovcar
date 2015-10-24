9c01b4fe2187267664f34c8bef35e03394fcd63d
MarkoIvanetic <manna.grad@gmail.com>
2015-10-24 03:36:46 +0200

    Gallery completed

--- js/myjs.js
+++ js/myjs.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     var video_array = [{
-        id: 'fLRWPpTnF3I',
+        id: 'ja2vYVgXu2c',
         naslov: 'Title1 extremely wide title that cannot be read easely'
     }, {
-        id: 'ja2vYVgXu2c',
+        id: 'fLRWPpTnF3I',
         naslov: 'Title2'
     }, {
         id: 'ohnkNMFSnAc',
@@ -26,11 +26,12 @@ $(document).ready(function() {
     setAspectRatio();
     jQuery(window).resize(setAspectRatio);
 
+    //Initial video
+     videoChange(video_array[0].id,0);
     // Generating thumbnails
-
     $.each(video_array, function(index, value) {
         $(".video-thumbnail-container")
-            .append('<div class="col-xs-6 col-sm-4 col-lg-3 video-thumbnail" video-id=' + value.id + '><img src="http://img.youtube.com/vi/' + value.id + '/hqdefault.jpg">' + '<div class="video-overlay"><p>' + value.naslov + '</p></div><h3>' + value.naslov + '</h3></div>');
+            .append('<div class="col-xxs-6 col-xs-6 col-sm-4 col-lg-3 video-thumbnail" video-id=' + value.id + '><img src="http://img.youtube.com/vi/' + value.id + '/hqdefault.jpg">' + '<div class="video-overlay"><p>' + value.naslov + '</p></div><h3>' + value.naslov + '</h3></div>');
     });
 
     //Thumbnail functionallity
@@ -38,9 +39,13 @@ $(document).ready(function() {
         $('.video-thumbnail img').css("background-color", "black");
         jQuery(this).find('img').css("background-color", "rgba(215,37,35,1)");
         var videoID = jQuery(this).attr("video-id");
-        $('.video-main-video iframe').attr('src', 'https://www.youtube.com/embed/' + videoID + '?theme=light&showinfo=0&iv_load_policy=3&showsearch=0&rel=0&autoplay=1')
+        videoChange(videoID,1);
     })
-
+    function videoChange (videoID, autoplay) {
+        var source = 'https://www.youtube.com/embed/' + videoID + '?theme=light&showinfo=0&iv_load_policy=3&showsearch=0&rel=0'
+        if (autoplay) source+="&autoplay=1";
+        $('.video-main-video iframe').attr('src', source);
+    }
     //Gallery
 if ( $( ".popup-gallery" ).length ) {
     $('.popup-gallery img').each(function() {
