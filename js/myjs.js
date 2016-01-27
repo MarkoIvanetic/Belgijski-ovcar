c5ab2afa4b620076bb5fa890849b3c6f70cf9a8c
MarkoIvanetic <manna.grad@gmail.com>
2016-01-27 21:23:47 +0100

    C

--- js/myjs.js
+++ js/myjs.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
         // SERVER SIDE ONLY, NOT LOCAL, DO NOT F****** DELETE **************************************
 
         function generateImages(classGallery, folder, alt) {
-            var dir = "images/photos/" + folder;
+            var dir = "images/photos/" + folder +"/";
             var fileextension = ".jpg";
             $.ajax({
                 //This will retrieve the contents of the folder if the folder is configured as 'browsable'
