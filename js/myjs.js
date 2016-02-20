4aa68e3f81e67946a07eeb47b8939484800e026b
MarkoIvanetic <manna.grad@gmail.com>
2016-02-20 12:13:13 +0100

    Anchor implemented into image preview

--- js/myjs.js
+++ js/myjs.js
@@ -68,24 +68,7 @@ $(document).ready(function() {
 
         // SERVER SIDE ONLY, NOT LOCAL, DO NOT F****** DELETE **************************************
 
-        function generateImages(classGallery, folder, alt) {
-            var dir = "images/photos/" + folder;
-            var fileextension = ".jpg";
-            $.ajax({
-                //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-                url: dir,
-                success: function(data) {
-                    //List all .png file names in the page
-                    $(data).find("a:contains(" + fileextension + ")").each(function() {
-                        var filename = this.href.replace(window.location.host, "").replace("http://", "");
-                        var popupImage = '<div class="col-lg-3 col-sm-4 col-xs-6 col-xxs-12 gallery-pad"><a class="col-xs-12 gallery-thumbnail" href="' + dir + filename + '" title="' + alt + '"><img src="' + dir + filename + '" alt="' + alt + '"></a></div>';
-                        $("." + classGallery).append(popupImage);
-                        //  $("body").append("<img src='" + dir + filename + "'>");
-                    });
-                }
-            });
-        }
-        generateImages("gero", "gero", "Gero - pas za parenje");
+        
 
 
         $('.popup-gallery img').each(function() {
