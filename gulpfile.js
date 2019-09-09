8819e7083e44f29f9f8f90b9cd3e82598e4d909d
Marko Ivanetic <marko.ivanetic@cloudsensesolutions.com>
2019-09-09 17:57:30 +0200

    Update folder structure and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,35 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch')
+var watch = require('gulp-watch');
+var minify = require('gulp-minify');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 var runSequence = require('run-sequence');
 var concatCss = require('gulp-concat-css');
 var autoprefixer = require('gulp-autoprefixer');
-var cleanCSS = require('gulp-clean-css');
 
 sass.compiler = require('node-sass');
 
 var path = {
-    css: 'css/**/*.css',
-    scss: 'css/**/*.scss',
+    css: 'src/css/**/*.css',
+    scss: 'src/css/**/*.scss',
+    tmp: 'temporary/',
     dist: 'dist/',
-    js: 'js/**/*.js',
-    img: 'images/**/*.*'
+    vendorjs: 'src/js/vendor/*.js',
+    customjs: 'src/js/*.js'
 };
 
-
+// *****************************************************
+gulp.task('sass', function () {
+  return gulp.src(path.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(path.tmp));
+});
+gulp.task('copy-css', function() {
+   gulp.src(path.css)
+   .pipe(gulp.dest(path.tmp));
+});
 gulp.task('build-css', function() {
-    return gulp.src(path.css)
+    return gulp.src(path.tmp + '/**/*.css')
         .pipe(concatCss("bundle.css"))
         .pipe(gulp.dest(path.dist));
 });
@@ -27,38 +38,38 @@ gulp.task('build-autoprefixer', function() {
         .pipe(autoprefixer())
         .pipe(gulp.dest(path.dist));
 });
-gulp.task('minify-css', () => {
-    return gulp.src(path.dist + 'bundle.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest(path.dist));
-});
 
 // *****************************************************
-
-gulp.task('compile', function(callback) {
-    return runSequence(
-        'build-css',
-        callback
-    );
-});
-
-gulp.task('sass', function () {
-  return gulp.src(path.scss)
-    .pipe(sass().on('error', sass.logError))
+gulp.task('build-vendor', function() {
+  return gulp.src(path.vendorjs)
+    .pipe(concat('vendor.js'))
     .pipe(gulp.dest(path.dist));
+});
+gulp.task('build-custom', function() {
+  return gulp.src(path.customjs)
+    .pipe(concat('custom.js'))
+    .pipe(gulp.dest(path.dist));
+});
+gulp.task('minify-vendor', function() {
+  gulp.src(path.dist+'vendor.js')
+    .pipe(minify())
+    .pipe(gulp.dest(path.dist))
+});
+// *****************************************************
+
+gulp.task('sass:watch', function () {
+  gulp.watch(path.scss, ['sass', 'copy-css', 'build-css']);
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch(path.scss, ['sass']);
+  gulp.watch(path.scss, ['sass', 'copy-css', 'build-css', 'build-autoprefixer']);
 });
 
-gulp.task('watch',['build-css'], function () {
-    gulp.watch(path.css, ['build-css'])
-});
 
 gulp.task('build', function(callback) {
-    return runSequence(
-        'build-css', 'build-autoprefixer', 'minify-css',
-        callback
-    );
+    return runSequence(['sass', 'copy-css', 'build-css', 'build-autoprefixer'], ['build-custom', 'minify-custom'], callback);
+});
+
+gulp.task('build-full', function(callback) {
+    return runSequence(['sass', 'copy-css', 'build-css', 'build-autoprefixer'], ['build-vendor', 'build-custom', 'minify-vendor'], callback);
 });
